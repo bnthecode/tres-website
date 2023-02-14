4550312103b7e145e60b0b7ff28fcca04ee04b2a
bnthecode <brandon.counts.personal@gmail.com>
2023-02-13 21:57:16 -0500

    pushing base project

---
+++ build/precache-manifest.34517942bcd99904ffa89348928f8382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0107d76d7c64b6085e87",
+    "url": "/static/css/main.afcbd434.chunk.css"
+  },
+  {
+    "revision": "0107d76d7c64b6085e87",
+    "url": "/static/js/main.0107d76d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbf5112c06ff30296e406400bc8fe108",
+    "url": "/index.html"
+  }
+];
